86f1e42cd1300800e9c2f19217ef9fedcb57eaee
Marios Athanasiou <90323650+marioathan99@users.noreply.github.com>
2022-11-16 19:45:26 +0200

    Create superPowers.java

---
+++ superPowers.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class SuperPower {
+	public static void main(String[] args){
+		 String power = JOptionPane.showInputDialog("What is your superpower?");
+		 power = power.toUpperCase();
+		 JOptionPane.showMessageDialog(null, power+" TO THE RESCUE!");
+         }
+}
